dfe278c96d72483a31bed40d1c62c932dea21beb
Andre <af349393@gmail.com>
2026-08-16 13:59:06 +0700

    feat: add cors

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,18 +1,30 @@
 package routes
 
 import (
-	"github.com/gin-gonic/gin"
+    "time"
+    "github.com/gin-contrib/cors"
+    "github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(r *gin.Engine) {
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello StunGuard",
-		})
-	})
-	BalitaRoute(r)
-	RegisterOrLoginPosyanduRoutes(r)
-	PosyanduRoute(r)
-	DinasKesehatanRoute(r)
-	PuskesmasRoute(r)
+    r.Use(cors.New(cors.Config{
+        AllowAllOrigins:  true,
+        AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true,
+        MaxAge:           12 * time.Hour,
+    }))
+
+    r.GET("/", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "message": "Hello StunGuard",
+        })
+    })
+    
+    BalitaRoute(r)
+    RegisterOrLoginPosyanduRoutes(r)
+    PosyanduRoute(r)
+    DinasKesehatanRoute(r)
+    PuskesmasRoute(r)
 }
